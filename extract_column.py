ea09f5860e2ce1f1478c0f3b3134c092cac456f9
Chih-Kai (Patrick) Wang <kk1fff@ckwang.info>
2016-03-05 17:47:46 +0800

    update extraction script

--- extract_column.py
+++ extract_column.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
-import sys
 
-col = int(sys.argv[2])
-with open(sys.argv[1], 'r') as f:
-    for l in f:
-        print(l.split('\t')[col])
+import sys, argparse
+
+class Collector:
+    def __init__(self, grouping):
+        if grouping:
+            self._set = set()
+        self._grouping = grouping
+    def collect(self, word):
+        if self._grouping:
+            if not word in self._set:
+                self._set.add(word)
+                print(word)
+        else:
+            print(word)
+
+def main():
+    aparse = argparse.ArgumentParser()
+    aparse.add_argument('filename', type=str)
+    aparse.add_argument('col', type=int)
+    aparse.add_argument('-g', nargs='?', const=True, default=False)
+    args = aparse.parse_args()
+    grouping = args.g
+    collector = Collector(grouping)
+    col = args.col
+
+    firstLine = True
+    with open(args.filename, 'r') as f:
+        for l in f:
+            if firstLine:
+                firstLine = False
+                continue
+            collector.collect(l.split('\t')[col])
+
+if __name__ == "__main__":
+    main()
